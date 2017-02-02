90d3c56bbccf0d62246f933764d9129062c930f6
kaladin9017 <sanchochris@gmail.com>
2017-02-02 16:42:46 -0500

    twitch setup

---
+++ src/components/widgets/twitch/TwitchWidget.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class TwitchWidget extends Component {
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ src/components/widgets/twitch/twitch-video-detail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TwitchVideoDetail = ({channel}) => {
+  return(
+    <div>
+      <iframe
+        src={`http://player.twitch.tv/?channel=${channel}`}
+        height="720"
+        width="1280"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen="true">
+    </iframe>
+    </div>
+  );
+
+};
+
+export default TwitchVideoDetail;
