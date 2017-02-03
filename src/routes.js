02916965e66d028db80db7ecec7233db2b59e4f7
kaladin9017 <sanchochris@gmail.com>
2017-02-03 12:44:38 -0500

    removed youtube widget from test

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Link , IndexRoute} from 'react-router';
 
 import Main from './components/Main';
-import YoutubeWidget from './components/widgets/youtube/YoutubeWidget';
 import TwitchWidget from './components/widgets/twitch/TwitchWidget';
 
 export default (
