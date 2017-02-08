8c2a011317bec0fb8d2d053cbfe7d9c67a702432
kaladin9017 <sanchochris@gmail.com>
2017-02-08 14:10:16 -0500

    route

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Link , IndexRoute } from 'react-router';
 
 import Main from './components/Main';
-import TwitchWidget from './components/widgets/twitch/TwitchWidget';
 import EveToken from './components/widgets/eve-mail/eve-token';
 import EveMail from './components/widgets/eve-mail/eve-mail';
 
+
 // FOR TESTING VIEW
 import FifteenGame from './components/widgets/fifteen-game/FifteenGame';
+import TwitchWidget from './components/widgets/twitch/TwitchWidget';
 //
+
 export default (
   <Route path="/" component={Main}>
     <Route path="/test" component={FifteenGame}/>
