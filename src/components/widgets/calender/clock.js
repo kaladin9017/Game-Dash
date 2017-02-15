6a62728760d3bde58d1ea1ec0b0a09a82ff93faf
kaladin9017 <sanchochris@gmail.com>
2017-02-15 17:37:20 -0500

    fixed index.html

--- src/components/widgets/calender/clock.js
+++ src/components/widgets/calender/clock.js
@@ -1,49 +1,49 @@
-import React from 'react';
-
-const Clock = React.createClass({
-  getInitialState(){
-    return{
-
-      hour: '',
-      minutes: '',
-      seconds: ''
-
-    }; 
-  },
-
-  componentDidMount(){
-    let today = new Date();
-    let hour = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    let startTime = () =>{
-      for(let i = 0; i < 0; i++){
-        
-      }
-    }; 
-   
-     
-    this.setState({
-      hour: hour, minutes: minutes, seconds: seconds
-    });
- 
-  },
-
-
-  render(){
-    // console.log(this.state);
-    if(this.state){
-    
-      return(
-        <div>
-       
-          {this.state.hour}:{this.state.minutes}:{this.state.seconds} 
-        </div>
-      );
-    }else{
-      <p> Loading...</p>;
-    }
-  }
-});
-
-export default Clock;
+// import React from 'react';
+//
+// const Clock = React.createClass({
+//   getInitialState(){
+//     return{
+//
+//       hour: '',
+//       minutes: '',
+//       seconds: ''
+//
+//     };
+//   },
+//
+//   componentDidMount(){
+//     let today = new Date();
+//     let hour = today.getHours();
+//     let minutes = today.getMinutes();
+//     let seconds = today.getSeconds();
+//     let startTime = () =>{
+//       for(let i = 0; i < 0; i++){
+//
+//       }
+//     };
+//
+//
+//     this.setState({
+//       hour: hour, minutes: minutes, seconds: seconds
+//     });
+//
+//   },
+//
+//
+//   render(){
+//     // console.log(this.state);
+//     if(this.state){
+//
+//       return(
+//         <div>
+//
+//           {this.state.hour}:{this.state.minutes}:{this.state.seconds}
+//         </div>
+//       );
+//     }else{
+//       <p> Loading...</p>;
+//     }
+//   }
+// });
+//
+// export default Clock;
