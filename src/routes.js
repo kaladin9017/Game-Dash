49c5a1672918dc33c7334e1033f50df22223c089
kaladin9017 <sanchochris@gmail.com>
2017-02-17 13:33:23 -0500

    fixed routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { Route, Link , IndexRoute } from 'react-router';
 
 // IMPORT COMPONENTS
 import Main from './components/Main';
-import InitialScreen from './components/dashboard/mainDashboard/mainDashboard';
 
 // IMPORT WIDGETS
 import EveToken from './components/widgets/eve-mail/eve-token';
@@ -29,13 +28,12 @@ import TwitchWidget from './components/widgets/twitch/TwitchWidget';
 
 export default (
   <Route path="/" component={Main}>
-		<IndexRoute component={InitialScreen}/>
 		<Route path="/armory" component={WowArmory}/>
 		<Route path="/relmstatus" component={RelmStatusWidget}/>
     <Route path="/youtube" component={YoutubeWidget}/>
     <Route path="/twitch" component={TwitchWidget}/>
     <Route path="/evetoken" component={EveToken}/>
-    <Route path="/evemail" component={EveMail}/>
+    <Route path="/eveMail" component={EveMail}/>
     <Route path="/tv" component={TvWidget}/>
     <Route path="/food" component={FoodWidget}/>
     <Route path="/giant" component={GiantBombWidget}/>
